d8d21e7740b443a4beafe1182cdf1bef5141be19
srg74 <28492985+srg74@users.noreply.github.com>
2025-08-21 09:14:49 -0400

    Minor update

--- include/config.h
+++ include/config.h
@@ -2,7 +2,7 @@
 
 // Build Information (auto-generated during compilation)
 #ifndef BUILD_DATE
-#define BUILD_DATE "2508180"  // YYMMDDx format x = daily build counter
+#define BUILD_DATE "2508210"  // YYMMDDx format x = daily build counter
 #endif
 
 #ifndef BUILD_TYPE
